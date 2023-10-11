eb2eabc35f1c807fc745364f5bc7a9029bce8cdb
sk_24 <sakshi.kalra_c@beeutech.com>
2023-10-11 17:02:12 +0530

    Javascript Regex

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import LearnRegex from './Regex';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <LearnRegex />
   </React.StrictMode>
 );
 
